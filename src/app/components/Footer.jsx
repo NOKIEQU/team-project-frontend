23d11f7f0d6a0a7de343f6c2e6876efce92207b8
Afnan Abdulkadir <afnanadam7771@gmail.com>
2024-11-29 19:42:46 +0000

    small change to footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,41 +2,41 @@ import Link from "next/link";
 
 function Footer() {
   return (
-<footer className="w-full flex justify-between items-center p-2 bg-gray-900 text-white font-sans  bottom-0 left-0">
-<div className="flex items-center space-x-4">
-       
-        <div className="text-2xl font-bold">GAME VAULT</div>
-            
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-grow">
+        {/* Main Content Here */}
       </div>
-      <div className="h-8 border-l-2 border-white mx-4"></div> 
-
-
-      <ul className="flex space-x-12">
+      <footer className="w-full flex justify-between items-center p-2 bg-gray-900 text-white font-sans">
+        <div className="flex items-center space-x-4">
+          <div className="text-2xl font-bold">GAME VAULT</div>
+        </div>
+        <div className="h-8 border-l-2 border-white mx-4"></div>
         
-        <li>
-          <Link href="/about" className="hover:text-orange-500 transition-all">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" className="hover:text-orange-500 transition-all">
-            Contact Us
-          </Link>
-        </li>
-        <li>
-          <Link href="/support" className="hover:text-orange-500 transition-all">
-            Support
-          </Link>
-        </li>
-       
-      </ul>
-      <div className="h-8 border-l-2 border-white mx-4"></div> 
-      <div className="hover:text-orange-500 transition-all">
-        <p>&copy; Copyright 2024 @Team 12</p>
-    
-
-      </div>
-    </footer>
+        <ul className="flex space-x-12">
+          <li>
+            <Link href="/about" className="hover:text-orange-500 transition-all">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="hover:text-orange-500 transition-all">
+              Contact Us
+            </Link>
+          </li>
+          <li>
+            <Link href="/support" className="hover:text-orange-500 transition-all">
+              Support
+            </Link>
+          </li>
+        </ul>
+        
+        <div className="h-8 border-l-2 border-white mx-4"></div>
+        
+        <div className="hover:text-orange-500 transition-all">
+          <p>&copy; Copyright 2024 @Team 12</p>
+        </div>
+      </footer>
+    </div>
   );
 }
 
