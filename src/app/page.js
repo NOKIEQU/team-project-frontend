b5ba32a251bf21e8db07299e096f0dac614d46af
Afnan Abdulkadir <afnanadam7771@gmail.com>
2024-12-01 11:45:56 +0000

    backround-colour

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full h-screen items-center justify-center gap-y-10">
+    <div className="flex flex-col w-full h-screen items-center justify-center gap-y-10 bg-gray-800">
       <Image
         src="/logo.svg"
         alt="Picture of the author"
         width={500}
         height={500}
       />
+
+
       <h1 className="text-4xl font-bold">Welcome to Game Vault</h1>
       <p className="text-lg">The website is under construction, please visit later!</p>
       <div className="flex flex-row justify-center items-center w-full gap-x-7 text-yellow-400 font-bold">
