a3051394467f427ed76ba1b314b51aa52fff2beb
Afnan Abdulkadir <afnanadam7771@gmail.com>
2024-12-01 04:02:27 +0000

    Questionnaire basic

--- src/app/Questionnaire/page.js
+++ src/app/Questionnaire/page.js
@@ -1,14 +1,88 @@
-function QuestionnairePage () {
-
-return (
-<div>
-<h1 className=''>  Welcome User</h1>
-
-<p>Q1</p>
-<button> Next </button>
+function Questionnaire() {
+   return (
+      <div>
+     <div className="min-h-screen bg-gradient-to-b from-gray-800 to-gray-900 text-white text-center">
+       <img className="w-20" src="/logo.svg" />
+       <h1 className="text-4xl font-serif font-bold pb-5">
+         Q1. Choose a genre you like:
+       </h1>
+ 
+       <div className="grid grid-cols-3 justify-items-center gap-2 mx-16 md:mx-24">
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           Action
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           Adventure
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-60 transition transform hover:scale-110">
+           Role-playing game
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           Horror
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           Shooter
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           Action
+         </button>
+       </div>
+ 
+       <div className="w-full bg-gray-300 h-2 rounded-full mx-auto mt-10">
+         <div className="bg-[#FFA800] h-2 rounded-full w-1/3"></div>
+       </div>
+ 
+       <h1 className="text-4xl font-serif font-bold pt-20 pb-10">
+         Q2. Choose your age category:
+       </h1>
+       <div className="grid grid-cols-3 justify-items-center gap-2 mx-16 md:mx-24">
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           under 12
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           12-17
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           18+
+         </button>
+       </div>
+ 
+       <div className="w-full bg-gray-300 h-2 rounded-full mx-auto mt-10">
+         <div className="bg-[#FFA800] h-2 rounded-full w-2/3"></div>
+       </div>
+ 
+       <h1 className="text-4xl font-serif font-bold pt-20 pb-10">
+         Q3. Do you prefer solo or multi-player games?
+       </h1>
+       <div className="grid grid-cols-3 justify-items-center gap-2 mx-16 md:mx-24">
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           Solo
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           Multi-player
+         </button>
+         <button className="m-4 bg-[#FFA800] hover:bg-orange-500 font-bold py-2 px-4 rounded-full w-40 transition transform hover:scale-110">
+           Both
+         </button>
+       </div>
+ <br></br>
+       <div className="w-full bg-gray-300 h-2 rounded-full mx-auto mt-10">
+         <div className="bg-[#FFA800] h-2 rounded-full w-full"></div>
+       </div>
+ 
+       <div className="flex items-center justify-end p-2 fixed bottom-10 right-10 bg-transparent">
+  <a href="/shop" className="flex items-center">
+    <img className="w-10 mr-2" src="/arrow.jpg" />
+    <p className="text-xl font-semibold">Continue</p>
+  </a>
 </div>
-)
 
-}
 
-export default QuestionnairePage;
+     </div>
+     
+   </div>
+   );
+ }
+ 
+ export default Questionnaire;
+ 
