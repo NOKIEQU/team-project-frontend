8c37b0256801602d902db579fdfaeb5f9e50764e
NOKIEQU <wiktorp92@gmail.com>
2024-12-04 22:06:04 +0000

    fixed game card

--- src/app/components/gameCard.jsx
+++ src/app/components/gameCard.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function GameCard(props) {
   return (
     <div className='flex flex-col bg-gray-800 text-white rounded-lg shadow-xl w-80 h-auto pb-10 transition-transform hover:scale-110'>
-      <Image src={props.image} alt={props.title} className="w-80 rounded-lg" />
+      <Image src={props.image} alt={props.title} width={320} height={200} className="w-80 rounded-lg" />
       <h1>{props.title}</h1>
       <p>{props.price}</p>
       <p>{props.description}</p>
