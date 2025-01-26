98ee368ac2d6e5a5731683c9bf3d66c017c62543
Muhammad <m.aazaan.126@gmail.com>
2025-01-26 17:49:19 +0000

    upadred

--- src/app/order history/layout.js
+++
@@ -1,9 +0,0 @@
-export default function OrderHistoryLayout({ children }) {
-    return (
-      <div className="w-full min-h-screen bg-[#0d1b2a]">
-        {children}
-      </div>
-    );
-  }
-
-  
